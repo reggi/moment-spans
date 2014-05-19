22b91ff3cb3dc112ee56ba103b41e0af95aaec76
reggi <thomas@reggi.com>
2014-05-19 15:15:31 -0400

    timing was off

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ var _date_spans = function(start_date, monthly_interval, question_date) {
   var date_spans = [];
 
   date_spans.push({
-    "start": moment(start_date),
-    "end": moment(start_date).add("days", monthly_interval - 2)
+    "start": moment(start_date).set('hour', 00).set('minute', 00).set('second', 00).set('millisecond', 00),
+    "end": moment(start_date).add("days", monthly_interval - 2).set('hour', 23).set('minute', 59).set('second', 59).set('millisecond', 59)
   })
 
   for (var i = 0; i < date_spans.length; i++) {
@@ -25,8 +25,8 @@ var _date_spans = function(start_date, monthly_interval, question_date) {
     var diff = question_date.diff(date_span.end);
     if (diff > 0) {
       date_spans.push({
-        "start": date_span.end.clone().add("days", 1),
-        "end": date_span.end.clone().add("month", 1)
+        "start": date_span.end.clone().add("days", 1).set('hour', 00).set('minute', 00).set('second', 00).set('millisecond', 00),
+        "end": date_span.end.clone().add("month", 1).set('hour', 23).set('minute', 59).set('second', 59).set('millisecond', 59)
       });
     }
   }
@@ -56,6 +56,7 @@ module.exports = function(start_date, monthly_interval, question_date) {
   start_date = _start_date(start_date);
   //if (question_date.diff(start_date) < 0) return new Error("The date provided is before the subscription start date, no batch number.");
   var date_spans = _date_spans(start_date, monthly_interval, question_date);
+  console.log(_format_spans(date_spans));
   var _span = 0;
   _.each(date_spans, function(date_span, span) {
     var isBetween = is_between(date_span, question_date);
